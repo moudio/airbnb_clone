5d309a547c2ddeffb2ad65caed4bfa519dd6ac0b
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-09-02 11:35:50 +0000

    Install material ui icons

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.css';
-
+import SearchIcon from '@material-ui/core/Icon/';
 function Header() {
   return (
     <div className="header">
@@ -9,6 +9,10 @@ function Header() {
         src="https://logos-world.net/wp-content/uploads/2020/07/Airbnb-Logo.png"
         alt=""
       />
+      <div className="header__center">
+        <input type="text" />
+        <SearchIcon />
+      </div>
     </div>
   );
 }
