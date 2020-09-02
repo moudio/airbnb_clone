7ed23ffc78f31c2fdb331fdeaf617c1b964075bf
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-09-02 14:35:13 +0000

    Create the search page component and make it a route

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import Header from './Header';
 
 import './App.css';
 import Footer from './Footer';
+import SearchPage from './SearchPage';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
-      <Header />
-      <Home />
-      <Footer />
-      {/* Header */}
-      {/* Banner */}
-
-      {/* Cards */}
-      {/* Footer */}
-      {/* Searchpage */}
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/SearchPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './SearchPage.css';
+function SearchPage() {
+  return (
+    <div className="searchPage">
+      <h3>I am a search page</h3>
+    </div>
+  );
+}
+
+export default SearchPage;
