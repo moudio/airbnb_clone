37f7c8a4dc199f4ffc5587505022881c706b722b
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-09-02 12:41:56 +0000

    SHow the date picker dynamically

--- src/Banner.js
+++ src/Banner.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
 import './Banner.css';
+import { Button } from '@material-ui/core';
 function Banner() {
+  const [showSearch, setShowSearch] = useState(false);
   return (
     <div className="banner">
+      <div className="banner__search">
+        {showSearch && <h1> Show date</h1>}
+        <Button
+          onClick={() => setShowSearch(!showSearch)}
+          variant="outlined"
+          className="banner__searchButton"
+        >
+          Search Dates
+        </Button>
+      </div>
       <div className="banner__info">
         <h1>Get out and strech your imagination</h1>
         <h5>
           Plan a different king of gateway to uncover the hidden gems near you
         </h5>
+        <Button variant="outlined">Explore Nearby</Button>
       </div>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,6 @@ import './Home.css';
 function Home() {
   return (
     <div className="home">
-      <h1>Home component</h1>
       <Banner />
     </div>
   );
