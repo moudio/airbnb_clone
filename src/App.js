dd2987730ff93c4e4a27d75e63c0a2fdf7711673
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-09-02 14:26:49 +0000

    Install react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Home from './Home';
 import Header from './Header';
 
 import './App.css';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="app">
       <Header />
       <Home />
+      <Footer />
       {/* Header */}
       {/* Banner */}
 
--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import './Footer.css';
 function Footer() {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+      <p>© 2020 Airbnb clone! No rights reserved - this is a demo!</p>
+      <p>
+        Made with ❤️ by <a href="http://mouhadiouf.com/">Mouhamadou</a>
+      </p>
+    </div>
+  );
 }
 
 export default Footer;
