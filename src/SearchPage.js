9d9986ffee619cd1ceebc028b75b1d60df595673
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-09-02 15:14:21 +0000

    Create the search page buttons

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './SearchPage.css';
+import { Button } from '@material-ui/core';
 function SearchPage() {
   return (
     <div className="searchPage">
-      <h3>I am a search page</h3>
+      <div className="searchPage__info">
+        <p>62 Stays · 26 august to 30 august · 2 guests </p>
+        <h1>Stays nearby</h1>
+        <Button variant="outlined">Cancellation Flexibility</Button>
+        <Button variant="outlined">Type of place</Button>
+        <Button variant="outlined">Rooms and beds</Button>
+        <Button variant="outlined">Cancellation Flexibility</Button>{' '}
+        <Button variant="outlined">More filters</Button>
+      </div>
     </div>
   );
 }
